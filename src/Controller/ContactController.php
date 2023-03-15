aac0d878966da343af578a1d3a7bd94710a68986
Robin Rémy <remyrobin51@gmail.com>
2023-03-15 17:10:12 +0100

    AJOUT / MODIF : Remise au propre du projet

---
+++ src/Controller/ContactController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ContactRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Contact;
+
+class ContactController extends AbstractController
+{
+    #[Route('/contact/{id}', name: 'app_contact_show', requirements: ['contactId' => '\d+'])]
+    public function show(Contact $contact = null): Response
+    {
+        if(is_null($contact))
+            throw $this->createNotFoundException("Le contact cet identifiant n'existe pas.");
+        
+
+        return $this->render('contact/show.html.twig', ['contact' => $contact]);
+    }
+
+    #[Route('/contact', name: 'app_contact')]
+    public function index(ContactRepository $contactRepository): Response
+    {
+        $contacts = $contactRepository->findAll();
+
+        return $this->render('contact/index.html.twig', ['contacts' => $contacts]);
+    }
+}

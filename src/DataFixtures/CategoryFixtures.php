7393c918b4ee5ec12d3e1e0f4d05ab270a65b590
Robin Rémy <remyrobin51@gmail.com>
2023-03-25 12:48:15 +0100

    Ajout : Forge et generateur pour les categories

---
+++ src/DataFixtures/CategoryFixtures.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Factory\CategoryFactory;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class CategoryFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $datas = json_decode(file_get_contents(__DIR__.'/data/Category.json'));
+
+        foreach ($datas as $nameCategory) {
+            $category = CategoryFactory::createOne(["name" => $nameCategory->name]);
+        }
+    }
+}

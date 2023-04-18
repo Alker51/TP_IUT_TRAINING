7ef1e36cc5667279cac47747b58860c53aec3822
Robin Rémy <remyrobin51@gmail.com>
2023-04-18 14:00:50 +0200

    Installation de Symfony UX et debut exercice 21.1 methode crud et template crud

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -34,4 +34,21 @@ class ContactController extends AbstractController
 
 
     }
+    #[Route('/contact/{id}/update', name: 'app_contact_update', requirements: ['id' => '\d+'])]
+    public function update(Contact $contact) : Response
+    {
+        return $this->render('contact/update.html.twig', ['contact' => $contact]);
+    }
+
+    #[Route('/contact/{id}/delete', name: 'app_contact_delete', requirements: ['id' => '\d+'])]
+    public function delete(Contact $contact) : Response
+    {
+        return $this->render('contact/delete.html.twig', ['contact' => $contact]);
+    }
+
+    #[Route('/contact/create', name: 'app_contact_create')]
+    public function create() : Response
+    {
+        return $this->render('contact/create.html.twig');
+    }
 }
